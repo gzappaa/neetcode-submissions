f2e46fb0447525a9a96b45d73659e374f24c0af9
Giovanni Zappa <gzappazg@gmail.com>
2026-06-29 09:03:51 +0300

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n == 0:
+            return 1
+
+        if n == 1:
+            return 1
+
+        prev, cur = 1, 1
+
+        for i in range(2, n+1):
+            prev, cur = cur, prev + cur
+
+        return cur 
+        
